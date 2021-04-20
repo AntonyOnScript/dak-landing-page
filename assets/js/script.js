61dcf24407161c1f533ffe874d1d2ff9f0254b28
AntonyOnScript <santhiagoafk@gmail.com>
2021-04-20 18:17:29 -0300

    slide com video pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 let slide_01_01 = document.querySelector('.slide-01-01')
 let slide_01_02 = document.querySelector('.slide-01-02')
 let slide_01_03 = document.querySelector('.slide-01-03')
+let slide_01_04 = document.querySelector('.slide-01-04')
 
 let area_botao_slider_01_01 = document.querySelector('.area-botao-slider-01-01')
 let area_botao_slider_01_02 = document.querySelector('.area-botao-slider-01-02')
 let area_botao_slider_01_03 = document.querySelector('.area-botao-slider-01-03')
+let area_botao_slider_01_04 = document.querySelector('.area-botao-slider-01-04')
 
 let botao_slider_01_01 = document.querySelector('.botao-slider-01-01')
 let botao_slider_01_02 = document.querySelector('.botao-slider-01-02')
 let botao_slider_01_03 = document.querySelector('.botao-slider-01-03')
+let botao_slider_01_04 = document.querySelector('.botao-slider-01-04')
 
 let slide_02_01 = document.querySelector('.slide-02-01')
 let slide_02_02 = document.querySelector('.slide-02-02')
@@ -31,7 +34,7 @@ before_numero.dataset.content = '01'
 
 document.addEventListener('click', passaSlide)
 
-passaSlideAutomatico(slide_01_03, slide_01_01, slide_01_02)
+passaSlideAutomatico(slide_01_03, slide_01_01, slide_01_02, slide_01_04)
 passaSlideAutomatico(slide_02_03, slide_02_01, slide_02_02)
 
 //funções
@@ -40,8 +43,8 @@ function passaSlide(e){
 }
 // a cada 5.5 segundos passa o slide do hero automaticamente
 
-const intervaloSlide01 = setInterval(() => {
-    passaSlideAutomatico(slide_01_01, slide_01_02, slide_01_03)
+var intervaloSlide01 = setInterval(() => {
+    passaSlideAutomatico(slide_01_01, slide_01_02, slide_01_03, slide_01_04)
 }, 5500);
 
 const intervaloSlide02 = setInterval(() => {
@@ -83,33 +86,54 @@ function checkBefore(){
         before_numero.dataset.content = '02'
     }else if(slide_01_03.style.display!=="none"){
         before_numero.dataset.content = '03'
+    }else if(slide_01_04.style.display!=="none"){
+        before_numero.dataset.content = '04'
     }
 }
 
 // passa o slide automático
-function passaSlideAutomatico(slide01, slide02, slide03){
-    if(slide01.style.display!=="none"){
-        passaSlideProLado(slide02, slide03, slide01)
-        checkBefore()
-    }else if(slide02.style.display!=="none"){
-        passaSlideProLado(slide03, slide02, slide01)
-        checkBefore()
-    }else if(slide03.style.display!=="none"){
-        passaSlideProLado(slide01, slide02, slide03)
-        checkBefore()
+function passaSlideAutomatico(slide01, slide02, slide03, slide04){
+    if(typeof slide04 !== "undefined"){
+        if(slide01.style.display!=="none"){
+            passaSlideProLado(slide02, slide03, slide01, slide04)
+            checkBefore()
+        }else if(slide02.style.display!=="none"){
+            passaSlideProLado(slide03, slide02, slide01, slide04)
+            checkBefore()
+        }else if(slide03.style.display!=="none"){
+            passaSlideProLado(slide04, slide02, slide03, slide01)
+            checkBefore()
+        }else if(slide04.style.display!=="none"){
+            passaSlideProLado(slide01, slide02, slide03, slide04)
+            checkBefore()
+        }
+    }else{
+        if(slide01.style.display!=="none"){
+            passaSlideProLado(slide02, slide03, slide01)
+            checkBefore()
+        }else if(slide02.style.display!=="none"){
+            passaSlideProLado(slide03, slide02, slide01)
+            checkBefore()
+        }else if(slide03.style.display!=="none"){
+            passaSlideProLado(slide01, slide02, slide03)
+            checkBefore()
+        }
     }
 }
 
 // ação dos botoes no slide
 function acaoDosBotoes(elemento){
     if(elemento.classList.contains('area-botao-slider-01-01') || elemento.classList.contains('botao-slider-01-01')){
-        passaSlideProLado(slide_01_01, slide_01_02, slide_01_03)
+        passaSlideProLado(slide_01_01, slide_01_02, slide_01_03, slide_01_04)
     }
     if(elemento.classList.contains('area-botao-slider-01-02') || elemento.classList.contains('botao-slider-01-02')){
-        passaSlideProLado(slide_01_02, slide_01_01, slide_01_03)
+        passaSlideProLado(slide_01_02, slide_01_01, slide_01_03, slide_01_04)
     }
     if(elemento.classList.contains('area-botao-slider-01-03') || elemento.classList.contains('botao-slider-01-03')){
-        passaSlideProLado(slide_01_03, slide_01_02, slide_01_01)
+        passaSlideProLado(slide_01_03, slide_01_02, slide_01_01, slide_01_04)
+    }
+    if(elemento.classList.contains('area-botao-slider-01-04') || elemento.classList.contains('botao-slider-01-04')){
+        passaSlideProLado(slide_01_04, slide_01_02, slide_01_01, slide_02_03)
     }
 
     if(elemento.classList.contains('area-botao-slider-02-01') || elemento.classList.contains('botao-slider-02-01')){
@@ -130,27 +154,45 @@ function acaoDosBotoes(elemento){
     }
 
     if(elemento.classList.contains('seta-03')){
-        passaSlideAutomatico(slide_01_02, slide_01_01, slide_01_03)
+        if(!slide_01_04.paused){
+            slide_01_04.pause()
+        }
+        passaSlideAutomatico(slide_01_02, slide_01_01, slide_01_04, slide_01_03)
     }
     if(elemento.classList.contains('seta-04')){
-        passaSlideAutomatico(slide_01_01, slide_01_02, slide_01_03)
+        if(!slide_01_04.paused){
+            slide_01_04.pause()
+        }
+        passaSlideAutomatico(slide_01_01, slide_01_02, slide_01_03, slide_01_04)
     }
 }
 
-function passaSlideProLado(source_01, source_02, source_03){
+function passaSlideProLado(source_01, source_02, source_03, source_04){
     if(source_01.classList.contains('slide-01-01')){
-        resetarCoresSlide(botao_slider_01_01, botao_slider_01_02, botao_slider_01_03)
+        resetarCoresSlide(botao_slider_01_01, botao_slider_01_02, botao_slider_01_03, botao_slider_01_04)
         mudaCorDoBotãoAutomático(botao_slider_01_01)
     }
     if(source_01.classList.contains('slide-01-02')){
-        resetarCoresSlide(botao_slider_01_01, botao_slider_01_02, botao_slider_01_03)
+        resetarCoresSlide(botao_slider_01_01, botao_slider_01_02, botao_slider_01_03, botao_slider_01_04)
         mudaCorDoBotãoAutomático(botao_slider_01_02)
     }
     if(source_01.classList.contains('slide-01-03')){
-        resetarCoresSlide(botao_slider_01_01, botao_slider_01_02, botao_slider_01_03)
+        resetarCoresSlide(botao_slider_01_01, botao_slider_01_02, botao_slider_01_03, botao_slider_01_04)
         mudaCorDoBotãoAutomático(botao_slider_01_03)
     }
-
+    if(source_01.classList.contains('slide-01-04')){
+        resetarCoresSlide(botao_slider_01_01, botao_slider_01_02, botao_slider_01_03, botao_slider_01_04)
+        mudaCorDoBotãoAutomático(botao_slider_01_04)
+        clearInterval(intervaloSlide01)
+        slide_01_04.play()
+        slide_01_04.onended = function(){
+            intervaloSlide01 = setInterval(() => {
+                passaSlideAutomatico(slide_01_01, slide_01_02, slide_01_03, slide_01_04)
+            }, 5500);
+            passaSlideAutomatico(slide_01_04, slide_01_01, slide_01_02, slide_01_03)
+        }
+    }
+    
     if(source_01.classList.contains('slide-02-01')){
         resetarCoresSlide(botao_slider_02_01, botao_slider_02_02, botao_slider_02_03)
         mudaCorDoBotãoAutomático(botao_slider_02_01)
@@ -165,9 +207,13 @@ function passaSlideProLado(source_01, source_02, source_03){
     }
     
     source_02.style.animation = source_03.style.animation = "animacao 1s"
+    if(typeof source_04 !== "undefined") {
+        source_04.style.animation = "animacao 1s"
+        source_04.style.display = "none"
+    }
     source_02.style.display = source_03.style.display = 'none'
-    source_01.style.display = 'flex'
 
+    source_01.style.display = 'flex'
     checkBefore()
 }
 
@@ -175,8 +221,11 @@ function mudaCorDoBotãoAutomático(botao){
     botao.style.backgroundColor = "var(--cor-primaria)"
 }
 
-function resetarCoresSlide(botao_01, botao_02, botao_03){
+function resetarCoresSlide(botao_01, botao_02, botao_03, botao_04){
     botao_01.style.backgroundColor = botao_02.style.backgroundColor = botao_03.style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+    if(typeof botao_04!=="undefined"){
+        botao_04.style.backgroundColor = "rgba(0, 0, 0, 0.1)"
+    }
 }
 
 // estilo
