96ac15c8d1204a08f430f4ba888e8d7390edc9c3
Antony <62514607+AntonyOnScript@users.noreply.github.com>
2021-04-24 14:43:41 -0300

    Consertado agora mascara do telefone funciona em celulares

--- assets/js/script.js
+++ assets/js/script.js
@@ -303,7 +303,7 @@ function fechaModal(){
 // mascara telefone
 
 let telefone_input = document.querySelector('#telefone')
-telefone_input.addEventListener('keypress', (e)=>{
+telefone_input.addEventListener('keydown', (e)=>{
     let numeros_permitidos = ['0','1','2','3','4','5','6','7','8','9','-','(',')']
 
     if(numeros_permitidos.indexOf(e.key) === -1){
