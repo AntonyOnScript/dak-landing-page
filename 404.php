c4a98b206cb98289e6679ebbb6cb01c8c34bf633
AntonyOnScript <santhiagoafk@gmail.com>
2021-05-04 19:34:29 -0300

    atualizado index.html

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php require_once 'header.php'; ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			<!-- article -->
+			<article id="post-404">
+
+				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
+				<h2>
+					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
+				</h2>
+
+			</article>
+			<!-- /article -->
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php require_once 'footer.php';
