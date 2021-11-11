0ed3a230f8964cf06f90a73d9b3a847c81cc1aee
AntonyOnScript <santhiagoafk@gmail.com>
2021-11-11 09:44:36 -0300

    Revert "Removeu todo o código que havia sobrado relacionado ao botão de pesquisa"
    This reverts commit 12fa2ec5fe6f048459c68bda8220ab6347ef75b0.

---
+++ assets/js/pesquisa.js
@@ -0,0 +1,27 @@
+const lupa = document.querySelectorAll('.pesquisa')
+const lupa2 = document.querySelector('.lupa')
+const caixa_de_pesquisa = document.querySelector('.modal-pesquisa')
+const fecha_pesquisa = document.querySelector('.fecha-pesquisa-modal')
+
+fecha_pesquisa.addEventListener('click', ()=>{
+    caixa_de_pesquisa.style.display = 'none'
+})
+
+lupa2.onmouseenter = ()=>{
+    lupa2.querySelector('.circulo-01').style.display = 'flex'
+    lupa2.querySelector('.circulo-01').style.animation = 'lupa .3s'
+}
+
+lupa2.onmouseleave = ()=>{
+    lupa2.querySelector('.circulo-01').style.animation = 'lupaInverso .3s'
+    setTimeout(()=>{
+        lupa2.querySelector('.circulo-01').style.display = 'none'
+    }, 300)
+}
+
+lupa2.onclick = abrirPesquisa
+lupa[0].addEventListener('click', abrirPesquisa)
+lupa[1].addEventListener('click', abrirPesquisa)
+function abrirPesquisa(){
+    caixa_de_pesquisa.style.display="flex"
+}
